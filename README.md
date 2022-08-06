4e01569d09fc3f45d0569caa4ee875bfdc81b833
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-06 18:34:48 +0530

    Birthday remibder completed

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Birthday reminder Project
+
+Remove Button to remove certain Item from the List
+
+Clear aLL button to remove all items from the list
+
+Project is focused on useState hook of Rect JS
--- src/List.js
+++ src/List.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
-const List = () => {
+const List = ({ people, setPeople }) => {
+  const handleRemove = (id) => {
+    setPeople((prevValue) => {
+      const newVal = prevValue.filter((man) => man.id !== id);
+      return newVal;
+    });
+  };
+
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        const { named, id, image, age } = person;
+        return (
+          <article className="person" key={id}>
+            <img src={image} alt={named} />
+            <div>
+              <h4>{named}</h4>
+              <p>{age}</p>
+              <button
+                style={{
+                  backgroundColor: 'purple',
+                  marginBlock: '1rem',
+                }}
+                onClick={() => handleRemove(id)}
+              >
+                Remove from List
+              </button>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
